e39713306e51d40c0f504a2c76a3ea4d964957af
MariosGramm <mariosgramm21@gmail.com>
2025-09-16 14:43:22 +0300

    Added more functions in JS file

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded',() => {
+document.addEventListener('DOMContentLoaded',function() {
   let debounceTimeout = null ;
 
   const searchInput = document.getElementById("searchInput")
@@ -22,8 +22,8 @@ async function fetchBookFromApi(title) {
     if (!response.ok){
       throw new Error('Invalid Response')
     }else {
-      const data = response.json()
-      handleReponse(data)
+      const data = await response.json()
+      handleResponse(data)
     }
   }catch (error) {
     console.error("Error: " + error)
@@ -31,13 +31,48 @@ async function fetchBookFromApi(title) {
   }
 }
 
-function handleReponse() {
-
+function handleResponse(data) {
+  if (data.totalItems > 0 ){
+    hideComponent("#LoadingGIF")
+    render(data)
+  }else {
+    hideComponent("#LoadingGIF")
+    showComponent("#NotFound")
+  }
 }
 
 function onBeforeSend() {
-  showComponent("LoadingGIF")
-  hideComponent("NotFound")
-  hideComponent("Error")
+  showComponent("#LoadingGIF")
+  hideComponent("#NotFound")
+  hideComponent("#Error")
+}
+
+function onApiError(){
+  showComponent("#Error")
+  showComponent("#NotFound")
+}
+
+function showComponent(component){
+  const element = document.querySelector(component)
+  if (element) element.classList.remove("hidden")
+}
+
+function hideComponent(component){
+  const element = document.querySelector(component)
+  if (element) element.classList.add("hidden")
+}
+
+function render(data) {
+  const bookLink = document.getElementById("bookLink")
+  bookLink.href = `https://www.google.gr/books/edition/${data.title}/${data.id}?hl=${data.language}&gbpv=1`
+
+  document.getElementById("BookTitle").textContent = data.items[0].volumeInfo.title
+  document.getElementById("BookAuthor").textContent = data.items[0].volumeInfo.authors
+  document.getElementById("DatePublished").textContent = data.items[0].volumeInfo.publishedDate
+  document.getElementById("BookDescription").textContent = data.items[0].volumeInfo.description
+  
+  const bookCover = document.getElementById("BookImage")
+  bookCover.src = data.items[0].volumeInfo.imageLinks.thumbnail
+  bookCover.alt = data.items[0].volumeInfo.title
 }
 

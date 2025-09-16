b6c1301cd283984e31d57ca233169c6a14d5dca0
MariosGramm <mariosgramm21@gmail.com>
2025-09-16 04:40:00 +0300

    Added functions in js file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,43 @@
 document.addEventListener('DOMContentLoaded',() => {
-  
+  let debounceTimeout = null ;
+
+  const searchInput = document.getElementById("searchInput")
+  searchInput.addEventListener('input', () => {
+    clearTimeout(debounceTimeout)
+    debounceTimeout = setTimeout(() => getBook(this.value.trim(),1500))
+  })
 })
+
+function getBook(title){
+  if (!title) return;
+
+  onBeforeSend();
+  fetchBookFromApi(title);
+}
+
+async function fetchBookFromApi(title) {
+  try{
+    let response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=intitle=${title}`)
+
+    if (!response.ok){
+      throw new Error('Invalid Response')
+    }else {
+      const data = response.json()
+      handleReponse(data)
+    }
+  }catch (error) {
+    console.error("Error: " + error)
+    onApiError()
+  }
+}
+
+function handleReponse() {
+
+}
+
+function onBeforeSend() {
+  showComponent("LoadingGIF")
+  hideComponent("NotFound")
+  hideComponent("Error")
+}
+
